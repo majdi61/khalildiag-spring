815199ee16d0404adff6191801a915a4a0cff89e
Maazoun majdi <maazoun.majdiii@gmail.com>
2024-06-08 13:10:19 +0100

    add rest

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +13,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	@RequestMapping("/")
+	public String test (){
+		return "working server on microsfot azure vps";
+	}
 }
