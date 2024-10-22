2ccccc5dd75b26f7dd89fc080cad46af105d490b
Maazoun majdi <maazoun.majdiii@gmail.com>
2024-10-22 19:49:11 +0100

    add allowed methode and local ip for testting

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -12,8 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://khalildiag-web-admin.web.app/","https://khalildiag-2a0e5.web.app/","https://khalildiag.com/")
-                .allowedMethods("PUT", "DELETE")
+                .allowedOrigins("https://khalildiag-web-admin.web.app/","https://khalildiag-2a0e5.web.app/","https://khalildiag.com/","http://127.0.0.1:5500/")
+                .allowedMethods("*")
                 .allowedHeaders("header1", "header2", "header3")
                 .exposedHeaders("header1", "header2")
                 .allowCredentials(false).maxAge(3600);
