3e8d8f6e8fa7f2fc9ea46067a194a6839cc1ed8f
Maazoun majdi <maazoun.majdiii@gmail.com>
2024-10-28 02:05:58 +0100

    extends SpringBootServletInitializer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication(scanBasePackages = "com.example.demo",exclude = {DataSourceAutoConfiguration.class})
 @EnableMongoRepositories(basePackages = "com.example.demo.repository")
 
-public class DemoApplication {
-
+public class DemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-
-	@RequestMapping("/")
-	public String test (){
-		return "working server on microsfot azure vps 1";
 	}
 }
