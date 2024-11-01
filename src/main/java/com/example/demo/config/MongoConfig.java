0f1be10fe1a4d9bf671ebedbc3dcad0c19cf0365
Maazoun majdi <maazoun.majdiii@gmail.com>
2024-11-01 11:00:13 +0100

    new db

--- src/main/java/com/example/demo/config/MongoConfig.java
+++ src/main/java/com/example/demo/config/MongoConfig.java
@@ -8,15 +8,15 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 @Configuration
 public class MongoConfig {
 
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(new SimpleMongoClientDatabaseFactory("mongodb+srv://admin:admin@cluster0.udh9u.mongodb.net/khalildiag?retryWrites=true&w=majority"));
-    }
-
-
 //    @Bean
 //    public MongoTemplate mongoTemplate() {
-//        String uri = "mongodb://yourUsername:yourPassword@57.129.70.159:27017/khalildiag?authSource=admin";
-//        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(uri));
+//        return new MongoTemplate(new SimpleMongoClientDatabaseFactory("mongodb+srv://admin:admin@cluster0.udh9u.mongodb.net/khalildiag?retryWrites=true&w=majority"));
 //    }
+
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        String uri = "mongodb://yourUsername:yourPassword@57.129.70.159:27017/khalildiag?authSource=admin";
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(uri));
+    }
 }
